3ac4a237df0b2b9d565774a9e43583ed44cebfc1
William Sheu <java.user.00001@gmail.com>
2012-12-01 14:56:06 -0800

    asdfg

---
+++ src/org/mvfbla/cgs2012/Game.java
@@ -0,0 +1,31 @@
+package org.mvfbla.cgs2012;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Game extends BasicGame {
+
+	public Game() {
+		super("one class barebone game");
+	}
+
+	public void init(GameContainer container) throws SlickException {
+		container.setTargetFrameRate(60); 
+	}
+
+	public void update(GameContainer container, int delta) throws SlickException {
+
+	}
+
+	public void render(GameContainer container, Graphics g)  {
+	}
+
+	public static void main(String[] argv) throws SlickException {
+		AppGameContainer container = 
+				new AppGameContainer(new Game(), 640, 480, false);
+		container.start();
+	}
+}
---
+++ src/org/mvfbla/cgs2012/Map.java
@@ -0,0 +1,10 @@
+package org.mvfbla.cgs2012;
+
+import org.newdawn.slick.tiled.TiledMap;
+
+public class Map {
+	private TiledMap map;
+	public Map(String tiledFile) {
+		
+	}
+}
