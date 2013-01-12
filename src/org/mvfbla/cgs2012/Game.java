fe1a0c553dfa710694fd979d02bb647f554d994f
Young <young.guo@gmail.com>
2013-01-12 12:32:32 -0800

    some changes

--- src/org/mvfbla/cgs2012/Game.java
+++ src/org/mvfbla/cgs2012/Game.java
@@ -1,11 +1,13 @@
 package org.mvfbla.cgs2012;
 
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
 
 public class Game extends BasicGame {
 
@@ -24,8 +26,10 @@ public class Game extends BasicGame {
 		container.setTargetFrameRate(30);
 		map = new Map("data\\TutorialLevel.tmx", "data");
 		GameConstants.currMap = map;
-		player = new Player(376, 585);
-		cameraBox = new CameraObject(player,132,132);
+		player = new Player(500, 496);
+		enemy = new Character(2100,508,32,32);
+		enemy.addAnimation("Enemy", new Animation(new SpriteSheet("data\\Bot.png", 32, 32), 150));
+		cameraBox = new CameraObject(player,250,250);
 	}
 
 	@Override
@@ -36,15 +40,15 @@ public class Game extends BasicGame {
 
 	@Override
 	public void render(GameContainer container, Graphics g)  {
+		g.setColor(Color.white);
+		//g.fillRect(0, 0, 1280, 720);
 		map.getMap().render((int)cameraBox.getOffsetX(),(int)cameraBox.getOffsetY());
-		//map.getMap().render(0,0);
 		cameraBox.draw(g);
 		g.setColor(Color.white);
-		g.drawRect(player.getX(),player.getY(),player.getWidth(),player.getHeight());
+		//g.drawRect(player.getX(),player.getY(),player.getWidth(),player.getHeight());
 		player.draw(g);
-		g.drawRect(cameraBox.getX(),cameraBox.getY(),cameraBox.getWidth(),cameraBox.getHeight());
-	//	for(Tile t : map.getBoxes())
-	//		g.draw(t.getCollision());
+		//g.drawRect(cameraBox.getX(),cameraBox.getY(),cameraBox.getWidth(),cameraBox.getHeight());
+		enemy.draw(g);
 	}
 
 	public static void main(String[] argv) throws SlickException {
--- src/org/mvfbla/cgs2012/GameConstants.java
+++ src/org/mvfbla/cgs2012/GameConstants.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class GameConstants {
 	//public static Game game;
 	// The acceleration of gravity
-	public static final float GRAVITY = 0.15f;
+	public static final float GRAVITY = 0.2f;
 	public static ArrayList<GameObject> gameObjects = new ArrayList<GameObject>();
 	public static Map currMap;
 }
