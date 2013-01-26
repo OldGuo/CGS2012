1447b32b4b40b5dd7d79c131c44a69c8e71c2e2f
Young <young.guo@gmail.com>
2013-01-25 17:20:03 -0800

    questions working yeye

--- src/org/mvfbla/cgs2012/QuestionButton.java
+++ src/org/mvfbla/cgs2012/QuestionButton.java
@@ -1,6 +1,9 @@
 package org.mvfbla.cgs2012;
 
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 
 public class QuestionButton {
 	private final Image buttonNormal,buttonHover,buttonClick;
@@ -8,14 +11,46 @@ public class QuestionButton {
 	private final int length;
 	private final int height;
 	private boolean hover, clickDown;
-	public QuestionButton(Image button,Image hover,Image click,int xPos,int yPos,int l, int h){
+	private final int PADDING = 25;
+	private final String answerString;
+	private final int buttonID;
+	public QuestionButton(String answer,Image button,Image hover,Image click,int xPos,int yPos,int l, int h,int id){
+		answerString = answer;
 		x = xPos;
 		y = yPos;
 		length = l;
 		height = h;
+		buttonID = id;
 		buttonNormal = button;
 		buttonHover = hover;
 		buttonClick = click;
+	}
+	public void draw(Graphics g,int offsetX,int offsetY){
+		if(getHover() == true)
+			g.drawImage(buttonHover,getX() + offsetX,getY() + offsetY);
+		else
+			g.drawImage(buttonNormal,getX() + offsetX,getY() + offsetY);
+		if(getClick() == true)
+			g.drawImage(buttonClick,getX() + offsetX,getY() + offsetY);
+		g.drawString(answerString, getX() + PADDING + offsetX, getY() + PADDING + offsetY);
+	}
+	public void update(GameContainer gc, Input input,int correctAnswer){
+		if(input.getMouseX()>getX() && input.getMouseX()<getX() + getLength() && input.getMouseY() > getY()  && input.getMouseY() < getY() + getHeight()){
+			if(input.isMouseButtonDown(input.MOUSE_LEFT_BUTTON)){
+				setClick(true);
+				if(correctAnswer == getID()){
+					System.out.println("UR A BEAST");
+				}else{
+					System.out.println("U SUK");
+				}
+			}else{
+				setClick(false);
+			}
+			setHover(true);
+		 }else{
+			 setClick(false);
+			 setHover(false);
+		 }
 	}
 	public Image getNormalButton(){
 		return buttonNormal;
@@ -55,5 +90,8 @@ public class QuestionButton {
 	}
 	public int getHeight(){
 		return height;
+	}
+	public int getID(){
+		return buttonID;
 	}
 }
