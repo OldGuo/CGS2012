87a0f043b64e2e633e5df58c36a03e6c864b8e66
William Sheu <java.user.00001@gmail.com>
2013-02-03 19:59:20 -0800

    unused import

--- src/org/mvfbla/cgs2012/GameLevel.java
+++ src/org/mvfbla/cgs2012/GameLevel.java
@@ -8,7 +8,6 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
