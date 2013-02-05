16668ecdc304bfd1a5a824f4f99d32bebc86792f
William Sheu <java.user.00001@gmail.com>
2013-02-04 22:28:46 -0800

    blue boss fix

--- src/org/mvfbla/cgs2012/levels/BlueBossLevel.java
+++ src/org/mvfbla/cgs2012/levels/BlueBossLevel.java
@@ -155,6 +155,7 @@ public class BlueBossLevel extends GameLevel {
 
 	@Override
 	public void render(GameContainer container, StateBasedGame sbg,Graphics g) throws SlickException  {
+		draw(g);
 		g.setColor(Color.black);
 		if(platformBroken == false)
 			g.fillRect(5*16,18*16,16*39,16*2);
@@ -179,7 +180,6 @@ public class BlueBossLevel extends GameLevel {
 			g.setColor(c);
 			g.fillRect(0, 0, 100000, 100000);
 		}
-		draw(g);
 		if(beforeQuestions == true || afterQuestions == true){
 			try {
 				text.draw(g,0,0,720,80);
