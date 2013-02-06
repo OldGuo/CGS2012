674a698574de4a313fb763aec43aaf64cfc722c6
William Sheu <java.user.00001@gmail.com>
2013-02-05 20:55:01 -0800

    commented 3 more files

--- src/org/mvfbla/cgs2012/base/Tile.java
+++ src/org/mvfbla/cgs2012/base/Tile.java
@@ -4,23 +4,30 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 
 
+/**
+ * @author PenguinToast
+ * Represents a game tile
+ */
 public class Tile extends GameObject{
 	private static final long serialVersionUID = -3752843176428390925L;
 
+	/**
+	 * Constructs a new tile with the specified location and dimension
+	 * @param x - X location of tile
+	 * @param y - Y location of tile
+	 * @param width - width of tile
+	 * @param height - height of tile
+	 */
 	public Tile(int x, int y, int width, int height) {
 		super(x, y, width, height);
 	}
 
 	@Override
 	public void draw(Graphics g) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void update(GameContainer gc, int delta) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
--- src/org/mvfbla/cgs2012/base/TiledObject.java
+++ src/org/mvfbla/cgs2012/base/TiledObject.java
@@ -7,11 +7,12 @@ public class TiledObject {
 	private String type;
 	private HashMap<String, String> properties = new HashMap<String, String>();
 	/**
-	 * @param x
-	 * @param y
-	 * @param width
-	 * @param height
-	 * @param type
+	 * Creates a new TileObject with the specified dimensions
+	 * @param x - X value of object
+	 * @param y - Y value of object
+	 * @param width - width of object
+	 * @param height - height of object
+	 * @param type - type of object
 	 */
 	public TiledObject(int x, int y, int width, int height, String type) {
 		this.x = x;
@@ -20,39 +21,89 @@ public class TiledObject {
 		this.height = height;
 		this.type = type;
 	}
+	/**
+	 * Adds a property of the object
+	 * @param name - Name of the property to add
+	 * @param value - Value of added property
+	 */
 	public void addProperty(String name, String value) {
 		properties.put(name, value);
 	}
+	/**
+	 * Gets the height of this object
+	 * @return Height of this object
+	 */
 	public int getHeight() {
 		return height;
 	}
+	/**
+	 * Returns the value of the property with the given name
+	 * @param name - Name of the property to find
+	 * @return Value of the property to find, null if not found
+	 */
 	public String getProperty(String name) {
 		return properties.get(name);
 	}
+	/**
+	 * Gets the type of this object
+	 * @return The type of this object
+	 */
 	public String getType() {
 		return type;
 	}
+	/**
+	 * Gets the width of this object
+	 * @return The width of this object
+	 */
 	public int getWidth() {
 		return width;
 	}
+	/**
+	 * Gets the X position of this object
+	 * @return The X position of this object
+	 */
 	public int getX() {
 		return x;
 	}
+	/**
+	 * Gets the Y position of this object
+	 * @return THe Y position of this object
+	 */
 	public int getY() {
 		return y;
 	}
+	/**
+	 * Sets the height of this object
+	 * @param height - The new height
+	 */
 	public void setHeight(int height) {
 		this.height = height;
 	}
+	/**
+	 * Sets the type of this object
+	 * @param type - THe new type of this image
+	 */
 	public void setType(String type) {
 		this.type = type;
 	}
+	/**
+	 * Sets the width of this object
+	 * @param width - The new width of this image
+	 */
 	public void setWidth(int width) {
 		this.width = width;
 	}
+	/**
+	 * Sets the X position of this object
+	 * @param x - The new x of this object
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
+	/**
+	 * Sets the y position of this object
+	 * @param y - The new y of this object
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
