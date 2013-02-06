c92bc4df2e88527394e5fd3e06e417c548c9f28d
William Sheu <java.user.00001@gmail.com>
2013-02-05 17:54:20 -0800

    ok fixed blue boss

--- src/org/mvfbla/cgs2012/levels/BlueBossLevel.java
+++ src/org/mvfbla/cgs2012/levels/BlueBossLevel.java
@@ -59,99 +59,101 @@ public class BlueBossLevel extends GameLevel {
 	public void update(GameContainer container,StateBasedGame sbg,int delta) throws SlickException {
 		//Updates the level
 		updateMain(container, sbg, delta);
-		if(!afterQuestions){ //Handles the pre-boss sequence
-			GameConstants.level.player.setControl(false);
-		}
-		GameConstants.collidableObjects.add(platform);
-		if(platformBroken){
-			fallY+=7;
-		}
-		for(Characters guy : GameConstants.enemies) { //Handles the updating of the Blue Boss
-			float tempX=player.getCenterX()-guy.getCenterX();//calculates distance between player and enemy
-			double Xdist=Math.pow(tempX, 2);
-			double Ydist=Math.pow(player.getCenterY()-guy.getCenterY(), 2);
-			float totalDist=(float)Math.sqrt(Xdist+Ydist);
-			String name=guy.getClass().toString();
-			if(name.equals("class org.mvfbla.cgs2012.BlueBoss")){
-				BlueBoss boss = (BlueBoss)guy;
-				if(totalDist<boss.getSight()){
-					boss.changeSleep(true);
-					boss.setDirection(Math.signum(tempX));
-					boss.setSpeed(1*Math.signum(tempX));
-				}else{
-					boss.changeSleep(false);
+		if(!GameConstants.getPaused()) {
+			if(!afterQuestions){ //Handles the pre-boss sequence
+				GameConstants.level.player.setControl(false);
+			}
+			GameConstants.collidableObjects.add(platform);
+			if(platformBroken){
+				fallY+=7;
+			}
+			for(Characters guy : GameConstants.enemies) { //Handles the updating of the Blue Boss
+				float tempX=player.getCenterX()-guy.getCenterX();//calculates distance between player and enemy
+				double Xdist=Math.pow(tempX, 2);
+				double Ydist=Math.pow(player.getCenterY()-guy.getCenterY(), 2);
+				float totalDist=(float)Math.sqrt(Xdist+Ydist);
+				String name=guy.getClass().toString();
+				if(name.equals("class org.mvfbla.cgs2012.BlueBoss")){
+					BlueBoss boss = (BlueBoss)guy;
+					if(totalDist<boss.getSight()){
+						boss.changeSleep(true);
+						boss.setDirection(Math.signum(tempX));
+						boss.setSpeed(1*Math.signum(tempX));
+					}else{
+						boss.changeSleep(false);
 
-				}
-				if(player.getCenterX() >= boss.getStompX() && player.getCenterX() <= boss.getStompX() + 64){
-					if(player.getCenterY() >= boss.getStompY() && player.getCenterY() <= boss.getStompY() + 50){
-						player.setHealth(player.getHealth() - 1);
 					}
-				}
-				brokenCount = 0;
-				for(int i = 0; i < GameConstants.pillars.size();i++){
-					if(boss.getStompX() > GameConstants.pillars.get(i).getX() &&
-							boss.getStompX() < GameConstants.pillars.get(i).getX() + GameConstants.pillars.get(i).getWidth()){
-						if(GameConstants.pillars.get(i).isBroken() == false)	{
-							boss.setHealth(boss.getHealth() - 1);
-							GameConstants.pillars.get(i).setBroken(true);
+					if(player.getCenterX() >= boss.getStompX() && player.getCenterX() <= boss.getStompX() + 64){
+						if(player.getCenterY() >= boss.getStompY() && player.getCenterY() <= boss.getStompY() + 50){
+							player.setHealth(player.getHealth() - 1);
 						}
 					}
-					if(GameConstants.pillars.get(i).isBroken())
-						brokenCount++;
-					if(brokenCount == 3){
-						platformBroken = true;
+					brokenCount = 0;
+					for(int i = 0; i < GameConstants.pillars.size();i++){
+						if(boss.getStompX() > GameConstants.pillars.get(i).getX() &&
+								boss.getStompX() < GameConstants.pillars.get(i).getX() + GameConstants.pillars.get(i).getWidth()){
+							if(GameConstants.pillars.get(i).isBroken() == false)	{
+								boss.setHealth(boss.getHealth() - 1);
+								GameConstants.pillars.get(i).setBroken(true);
+							}
+						}
+						if(GameConstants.pillars.get(i).isBroken())
+							brokenCount++;
+						if(brokenCount == 3){
+							platformBroken = true;
+						}
+					}
+					if(platformBroken == true){
+						platform.setY(-100);
+						platform.setX(-100);
+						boss.setFalling(true);
+						transState = 2;
+						if((GameConstants.bossesDefeated & 4) != 4) {
+							GameConstants.playNum++;
+						}
+						GameConstants.lastBoss = 1;
+						GameConstants.bossesDefeated |= 4;
 					}
 				}
-				if(platformBroken == true){
-					platform.setY(-100);
-					platform.setX(-100);
-					boss.setFalling(true);
-					transState = 2;
-					if((GameConstants.bossesDefeated & 4) != 4) {
-						GameConstants.playNum++;
-					}
-					GameConstants.lastBoss = 1;
-					GameConstants.bossesDefeated |= 4;
+			}
+			if(beforeQuestions){ //Handles the pre-boss sequence
+				text.setText("The moment I enter, I sense the air of superiority emanating from the figure in the room. " +
+						"I want to ask it so many questions. I want to understand.  A stream of " +
+						"questions pour from my mouth. But it only responds with questions of its own." +
+						"                                       ");
+				if(text.isFinished() && beforeQuestions){
+					beforeQuestions = false;
+					needRestart = true;
+					questions.setAnswering(true);
 				}
 			}
-		}
-		if(beforeQuestions){ //Handles the pre-boss sequence
-			text.setText("The moment I enter, I sense the air of superiority emanating from the figure in the room. " +
-					"I want to ask it so many questions. I want to understand.  A stream of " +
-					"questions pour from my mouth. But it only responds with questions of its own." +
-					"                                       ");
-			if(text.isFinished() && beforeQuestions){
-				beforeQuestions = false;
-				needRestart = true;
-				questions.setAnswering(true);
+			if(questions.getAnswering() == false && !beforeQuestions){ //Handles the pre-boss sequence
+				afterQuestions = true;
 			}
-		}
-		if(questions.getAnswering() == false && !beforeQuestions){ //Handles the pre-boss sequence
-			afterQuestions = true;
-		}
-		if(afterQuestions == true){ //Handles the pre-boss sequence
-			text.setText("I am done with its games. I want answers now. Who am I? Why am I here?" +
-					" But there is no answer, this only" +
-					" seems to infuriate the figure...   ...   ...   ...   ...   " +
-					"                                       ");
-			if(needRestart){
-				text.restart();
-				needRestart = false;
+			if(afterQuestions == true){ //Handles the pre-boss sequence
+				text.setText("I am done with its games. I want answers now. Who am I? Why am I here?" +
+						" But there is no answer, this only" +
+						" seems to infuriate the figure...   ...   ...   ...   ...   " +
+						"                                       ");
+				if(needRestart){
+					text.restart();
+					needRestart = false;
+				}
 			}
-		}
-		if(beforeQuestions == true || afterQuestions == true) //Handles the pre-boss sequence
-			text.update(container, delta);
-		if(questions.getAnswering()){ //Handles the pre-boss sequence
-			questions.update(container);
-			GameConstants.level.player.setControl(false);
-		}
-		for(Characters guy : GameConstants.enemies) { //Handles the pre-boss sequence
-			String name=guy.getClass().toString();
-			if(name.equals("class org.mvfbla.cgs2012.BlueBoss")){
-				BlueBoss boss = (BlueBoss)guy;
-				if(afterQuestions == true){
-					boss.setAttacking(true);
-					GameConstants.level.player.setControl(true);
+			if(beforeQuestions == true || afterQuestions == true) //Handles the pre-boss sequence
+				text.update(container, delta);
+			if(questions.getAnswering()){ //Handles the pre-boss sequence
+				questions.update(container);
+				GameConstants.level.player.setControl(false);
+			}
+			for(Characters guy : GameConstants.enemies) { //Handles the pre-boss sequence
+				String name=guy.getClass().toString();
+				if(name.equals("class org.mvfbla.cgs2012.BlueBoss")){
+					BlueBoss boss = (BlueBoss)guy;
+					if(afterQuestions == true){
+						boss.setAttacking(true);
+						GameConstants.level.player.setControl(true);
+					}
 				}
 			}
 		}
